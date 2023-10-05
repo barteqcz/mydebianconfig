0f1f01b8bce22038140a31122304813700717d98
Bartek <118764028+barteqcz@users.noreply.github.com>
2023-10-05 21:52:51 +0200

    Update

--- main.sh
+++ main.sh
@@ -3,5 +3,6 @@
 sudo apt update
 sudo apt purge ifupdown -y
 sudo rm -rf /etc/network
-sudo apt install network-manager-gnome pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber nvidia-driver cinnamon bluez bluez-cups cups cups-browsed cups-bsd cups-client cups-common cups-core-drivers cups-daemon cups-filters-core-drivers cups-filters cups-ipp-utils cups-pk-helper cups-ppdc cups-server-common ghostscript gutenprint hplip slick-greeter lightdm-settings -y
+sudo apt install network-manager-gnome pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber nvidia-driver cinnamon bluez bluez-cups cups cups-browsed cups-bsd cups-client cups-common cups-core-drivers cups-daemon cups-filters-core-drivers cups-filters cups-ipp-utils cups-pk-helper cups-ppdc cups-server-common ghostscript gutenprint hplip slick-greeter lightdm-settings htop -y
 sudo apt purge zutty lightdm-gtk-greeter -y
+sudo sed -i '/greeter-hide-users=false/s/^/#/g' /etc/lightdm/lightdm.conf
