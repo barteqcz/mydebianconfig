6751ab018b909aacb84471ee325a48861f84c9c9
Bartek <118764028+barteqcz@users.noreply.github.com>
2023-10-05 21:56:25 +0200

    Update

--- main.sh
+++ main.sh
@@ -6,3 +6,13 @@ sudo rm -rf /etc/network
 sudo apt install network-manager-gnome pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber nvidia-driver cinnamon bluez bluez-cups cups cups-browsed cups-bsd cups-client cups-common cups-core-drivers cups-daemon cups-filters-core-drivers cups-filters cups-ipp-utils cups-pk-helper cups-ppdc cups-server-common ghostscript gutenprint hplip slick-greeter lightdm-settings htop -y
 sudo apt purge zutty lightdm-gtk-greeter -y
 sudo sed -i '/greeter-hide-users=false/s/^/#/g' /etc/lightdm/lightdm.conf
+while true; do
+    read -rp "Done. Reboot? [Y/n]" response
+    if [[ $response == "y" || $response == "Y" || $response == "" ]]; then
+        sudo reboot
+    elif [[ $response == "n" || $response == "N" ]]; then
+        :
+    else
+        echo "Invalid option."
+    fi
+done
