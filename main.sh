29abed7239f334107a8dd0be703530e3797e0d45
Bartek <118764028+barteqcz@users.noreply.github.com>
2023-10-07 10:52:15 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,10 +4,11 @@ sudo apt update
 sudo apt purge ifupdown -y
 sudo rm -rf /etc/network
 sudo apt install network-manager-gnome pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber nvidia-driver cinnamon bluez bluez-cups cups cups-browsed cups-bsd cups-client cups-common cups-core-drivers cups-daemon cups-filters-core-drivers cups-filters cups-ipp-utils cups-pk-helper cups-ppdc cups-server-common ghostscript gutenprint hplip slick-greeter lightdm-settings htop -y
-sudo apt purge zutty lightdm-gtk-greeter -y
+sudo apt purge zutty lightdm-gtk-greeter yelp malcontent -y
+sudo apt autoremove --purge -y
 sudo sed -i '/greeter-hide-users=false/s/^/#/g' /etc/lightdm/lightdm.conf
 while true; do
-    read -rp "Done. Reboot? [Y/n]" response
+    read -rp "Done. Reboot? [Y/n] " response
     if [[ $response == "y" || $response == "Y" || $response == "" ]]; then
         sudo reboot
     elif [[ $response == "n" || $response == "N" ]]; then
