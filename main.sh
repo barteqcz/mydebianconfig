11e062b3cadc490586c17c0bca43c58312af20e7
Bartek <118764028+barteqcz@users.noreply.github.com>
2023-10-07 11:52:36 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ sudo rm -rf /etc/network
 sudo apt install network-manager-gnome pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber nvidia-driver cinnamon bluez bluez-cups cups cups-browsed cups-bsd cups-client cups-common cups-core-drivers cups-daemon cups-filters-core-drivers cups-filters cups-ipp-utils cups-pk-helper cups-ppdc cups-server-common ghostscript hplip slick-greeter lightdm-settings htop -y
 sudo apt purge zutty lightdm-gtk-greeter yelp malcontent -y
 sudo apt autoremove --purge -y
-sudo sed -i '/greeter-hide-users=false/s/^/#/g' /etc/lightdm/lightdm.conf
+suso sed -i '/greeter-hide-users=false/s/^#//g' /etc/lightdm/lightdm.conf
 sudo rm /etc/grub.d/05_debian_theme
 sudo update-grub
 while true; do
